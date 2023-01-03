7cf92d81beb8d1fdc5e2f1633b9719b301e71d27
matyao132 <takumasa0814@gmail.com>
2023-01-03 20:26:13 +0900

    atcoder:0103

---
+++ practiceA/SomeSums.py
@@ -0,0 +1,11 @@
+n,a,b = map(int,input().split())
+sum_list = []
+
+for i in range(1,n+1):
+    str_n = str(i)
+    check_num = sum(list(map(int,str_n)))
+
+    if check_num >= a and check_num <= b:
+        sum_list.append(i)
+
+print(sum(sum_list))
