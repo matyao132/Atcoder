e3b2b4c6f912bc3a12d0042000143b92cf31084f
matyao132 <takumasa0814@gmail.com>
2023-01-04 20:58:38 +0900

    他社の回答例追記

--- practiceA/KagamiMochi.py
+++ practiceA/KagamiMochi.py
@@ -1,5 +1,6 @@
 # 問題：https://atcoder.jp/contests/abs/tasks/abc085_b
 
+# 自分の回答例
 n = int(input())
 mochies = []
 
@@ -9,3 +10,8 @@ for i in range(n):
 distinct_mochies = list(set(mochies))
 
 print(len(distinct_mochies))
+
+# ほかの回答例
+N = int(input())
+d = [input() for _ in range(N)]
+print(len(set(d)))
